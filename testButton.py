3874b3b803ea92a93cf5a52fa744670a154db22d
KingWille <56981641+KingWille@users.noreply.github.com>
2022-02-20 17:07:38 +0100

    Update testButton.py

--- testButton.py
+++ testButton.py
@@ -3,10 +3,4 @@ from signal import pause
 
 button = Button(27)
 
-while True:
-    if button.is_pressed:
-        print("Button is pressed")
-    else:
-        print("Button is not pressed")
-        
 pause()
