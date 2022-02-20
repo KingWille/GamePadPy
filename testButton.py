080db846c217577c5dde871f5e1de3e058849b2a
KingWille <56981641+KingWille@users.noreply.github.com>
2022-02-20 16:46:10 +0100

    Update testButton.py

--- testButton.py
+++ testButton.py
@@ -1,5 +1,12 @@
 from gpiozero import Button
+from signal import pause
 
 button = Button(27)
 
+while True:
+    if button.is_pressed:
+        print("Button is pressed")
+    else:
+        print("Button is not pressed")
+        
 pause()
