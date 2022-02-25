95e7a1c9ded11051dcd1aef9fb8770939db0e971
KingWille <56981641+KingWille@users.noreply.github.com>
2022-02-25 23:31:04 +0100

    Update GamePadTest.py

--- GamePadTest.py
+++ GamePadTest.py
@@ -26,7 +26,7 @@ vry_pos = ReadChannel(vry_channel)
 swt_val = ReadChannel(swt_channel)
 
 def signal_handler(signal, frame):
-        print 'Grattis, vi ses'
+        print ('Grattis, vi ses')
         sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 #print 'Press Ctrl+C to exit'
