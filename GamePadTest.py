1696863d60e63c082d6e7adade151d2732303bdc
KingWille <56981641+KingWille@users.noreply.github.com>
2022-02-26 00:00:43 +0100

    Update GamePadTest.py

--- GamePadTest.py
+++ GamePadTest.py
@@ -93,25 +93,22 @@ while True:
   if 300 <= vrx_pos <= 700:
    adc_x_up = False
    adc_x_down = False
-  elif adc_x > 700:
+  elif vrx_pos > 700:
    adc_x_up = True
    adc_x_down = False
-  elif adc_x < 300:
+  elif vrx_pos < 300:
    adc_x_up = False
    adc_x_down = True
 
 
 
-  adc_y = adc_y_cal -(adc.readADCSingleEnded(1, gain, sps) / adc_divider)
-  adc_y = adc_y*-1
-
-  if 300 <= adc_y <= 700:
+  if 300 <= vry_pos <= 700:
    adc_y_up = False
    adc_y_down = False
-  elif adc_y > 700:
+  elif vry_pos > 700:
    adc_y_up = True
    adc_y_down = False
-  elif adc_y <= 300:
+  elif vry_pos <= 300:
    adc_y_up = False
    adc_y_down = True
 
