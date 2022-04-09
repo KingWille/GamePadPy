9d4aea96d7eac8741a70a510b725e3f779417ff2
KingWille <56981641+KingWille@users.noreply.github.com>
2022-04-09 19:24:37 +0200

    Update GamePadTest.py

--- GamePadTest.py
+++ GamePadTest.py
@@ -36,19 +36,28 @@ signal.signal(signal.SIGINT, signal_handler)
 
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP) #B
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Y
-GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP) #X
-GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP) #A
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP) # JS0 BTN
+GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP) #A
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP) #B
+GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP) #X
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_UP) #Y
 
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP) #START
-GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP) #TOUCHSCREEN TAP
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP) #DPADUP
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP) #DPADDOWN
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP) #DPADLEFT
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP) #DPADRIGHT
 
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP) #L1
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP) #L2
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP) #R1
+GPIO.setup(1, GPIO.IN, pull_up_down=GPIO.PUD_UP) #R2
+
+GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP) #START
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP) #SELECT
+GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP) #HOME
 
-GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP) #RSB
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP) #LSB
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP) #L3
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP) #R3
+
 
 
 #subprocess.call(["sudo", "shutdown", "-h","now"])
@@ -57,20 +66,31 @@ GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP) #LSB
 
 device = uinput.Device([uinput.KEY_UP,uinput.KEY_DOWN,uinput.KEY_LEFT,uinput.KEY_RIGHT,
                         uinput.KEY_X, uinput.KEY_ENTER,uinput.KEY_J,uinput.KEY_K, uinput.KEY_L,
-                        uinput.KEY_H, uinput.KEY_I, uinput.KEY_O, uinput.KEY_Y,uinput.KEY_M,
+                        uinput.KEY_P, uinput.KEY_G, uinput.KEY_Z, uinput.KEY_Q, uinput.KEY_H, 
+                        uinput.KEY_I, uinput.KEY_O, uinput.KEY_Y,uinput.KEY_M,
                         uinput.KEY_A, uinput.KEY_B,uinput.KEY_C, uinput.KEY_V, uinput.KEY_N, 
                         uinput.KEY_W, uinput.KEY_S, uinput.KEY_D, uinput.KEY_F, uinput.ABS_X, uinput.ABS_Y])
 
-fire = False
-up = False
-down = False
-left = False
-right = False
-js0btn = False
+#Knappar
+A = False
+B = False
+X = False
+Y = False
+DPADUP = False
+DPADDOWN = False
+DPADLEFT = False
+DPADRIGHT = False
+
+L1 = False
+L2 = False
+L3 = False
+R1 = False 
+R2 = False
+R3 = False
 
 start = False
 select = False
-touch = False
+home = False
 
 #Main JS
 js0x = 0
@@ -103,10 +123,6 @@ adc_y_down1 = False
 
 adc_x_up1 = False
 adc_x_down1 = False
-
-
-rsb = False
-lsb = False
 
 while True:
     
@@ -242,88 +258,144 @@ while True:
 
 
 
-  if (not fire) and (not GPIO.input(17)):  # Fire button pressed
-    fire = True
-    #print("B pressed")
-    device.emit(uinput.KEY_B, 1) # Press Left Ctrl key
-  if fire and GPIO.input(17):  # Fire button released
-    fire = False
-    device.emit(uinput.KEY_B, 0) # Release Left Ctrl key
-
-
-  if (not up) and (not GPIO.input(18)):  # Up button pressed
-    up = True
-    #print("Y pressed")
-    device.emit(uinput.KEY_Y, 1) # Press Up key
-  if up and GPIO.input(18):  # Up button released
-    up = False
-    device.emit(uinput.KEY_Y, 0) # Release Up key
-
-
-  if (not down) and (not GPIO.input(27)):  # Down button pressed
-    down = True
-    #print("X pressed")
-    device.emit(uinput.KEY_X, 1) # Press Down key
-  if down and GPIO.input(27):  # Down button released
-    down = False
-    device.emit(uinput.KEY_X, 0) # Release Down key
-
-
-  if (not left) and (not GPIO.input(22)):  # Left button pressed
-    left = True
+  if (not A) and (not GPIO.input(2)):  # A button pressed
+    A = True
     #print("A pressed")
-    device.emit(uinput.KEY_A, 1) # Press Left key
-  if left and GPIO.input(22):  # Left button released
-    left = False
-    device.emit(uinput.KEY_A, 0) # Release Left key
-
-  if (not js0btn) and (not GPIO.input(23)):  # Left button pressed
-    js0btn = True
-    #print("JS0 pressed")
-    device.emit(uinput.KEY_C, 1) # Press Left key
-  if js0btn and GPIO.input(23):  # Left button released
-    js0btn = False
-    device.emit(uinput.KEY_C, 0) # Release Left key
+    device.emit(uinput.KEY_Enter, 1) # Press Left Ctrl key
+  if A and GPIO.input(2):  # A button released
+    A = False
+    device.emit(uinput.KEY_Enter, 0) # Release Left Ctrl key
 
 
-  if (not start) and (not GPIO.input(24)):  # Left button pressed
-    start = True
+  if (not B) and (not GPIO.input(3)):  # B button pressed
+    B = True
+    #print("B pressed")
+    device.emit(uinput.KEY_B, 1) # Press B key
+  if B and GPIO.input(3):  # B button released
+    B = False
+    device.emit(uinput.KEY_B, 0) # Release B key
+
+
+  if (not X) and (not GPIO.input(14)):  # X button pressed
+    X = True
+    #print("X pressed")
+    device.emit(uinput.KEY_X, 1) # Press X key
+  if X and GPIO.input(14):  # X button released
+    X = False
+    device.emit(uinput.KEY_X, 0) # Release X key
+
+
+  if (not Y) and (not GPIO.input(15)):  # Y button pressed
+    Y = True
+    #print("Y pressed")
+    device.emit(uinput.KEY_Y, 1) # Press Y key
+  if Y and GPIO.input(15):  # Y button released
+    Y = False
+    device.emit(uinput.KEY_Y, 0) # Release Y key
+
+  if (not DPADUP) and (not GPIO.input(17)):  # Left button pressed
+    DPADUP = True
+    #print("DPADUP pressed")
+    device.emit(uinput.KEY_K, 1) # Press Left key
+  if DPADUP and GPIO.input(17):  # Left button released
+    DPADUP = False
+    device.emit(uinput.KEY_K, 0) # Release Left key
+
+
+  if (not DPADDOWN) and (not GPIO.input(18)):  # Left button pressed
+    DPADDOWN = True
     #print("start pressed")
-    device.emit(uinput.KEY_V, 1) # Press Left key
-  if start and GPIO.input(24):  # Left button released
-    start = False
-    device.emit(uinput.KEY_V, 0) # Release Left key
+    device.emit(uinput.KEY_J, 1) # Press Left key
+  if DPADDOWN and GPIO.input(18):  # Left button released
+    DPADDOWN = False
+    device.emit(uinput.KEY_J, 0) # Release Left key
 
-  if (not touch) and (not GPIO.input(25)):  # Left button pressed
-    touch = True
+  if (not DPADLEFT) and (not GPIO.input(27)):  # Left button pressed
+    DPADLEFT = True
     #print("touch pressed")
-    device.emit(uinput.KEY_ENTER, 1) # Press Left key
-  if touch and GPIO.input(25):  # Left button released
-    touch = False
-    device.emit(uinput.KEY_N, 0) # Release Left key
+    device.emit(uinput.KEY_H, 1) # Press Left key
+  if DPADLEFT and GPIO.input(27):  # Left button released
+    DPADLEFT = False
+    device.emit(uinput.KEY_H, 0) # Release Left key
 
-  if (not lsb) and (not GPIO.input(16)):  # Left button pressed
-    lsb = True
+  if (not DPADRIGHT) and (not GPIO.input(22)):  # Left button pressed
+    DPADRIGHT = True
     #print("lsb pressed")
     device.emit(uinput.KEY_I, 1) # Press Left key
-  if lsb and GPIO.input(16):  # Left button released
-    lsb = False
+  if DPADRIGHT and GPIO.input(22):  # Left button released
+    DPADRIGHT = False
     device.emit(uinput.KEY_I, 0) # Release Left key
 
-  if (not rsb) and (not GPIO.input(20)):  # Left button pressed
-    rsb = True
+  if (not L1) and (not GPIO.input(23)):  # Left button pressed
+    L1 = True
     #print("rsb pressed")
     device.emit(uinput.KEY_O, 1) # Press Left key
-  if rsb and GPIO.input(20):  # Left button released
-    rsb = False
+  if L1 and GPIO.input(23):  # Left button released
+    L1 = False
     device.emit(uinput.KEY_O, 0) # Release Left key
 
-  if (not select) and (not GPIO.input(21)):  # Left button pressed
-    select = True
+  if (not L2) and (not GPIO.input(24)):  # Left button pressed
+    L2 = True
     #print("select pressed")
     device.emit(uinput.KEY_M, 1) # Press Left key
-  if select and GPIO.input(21):  # Left button released
-    select = False
+  if L2 and GPIO.input(24):  # Left button released
+    L2 = False
     device.emit(uinput.KEY_M, 0) # Release Left key
+ 
+  if (not L3) and (not GPIO.input(12)):  # Left button pressed
+    L3 = True
+    #print("select pressed")
+    device.emit(uinput.KEY_C, 1) # Press Left key
+  if L3 and GPIO.input(12):  # Left button released
+    L3 = False
+    device.emit(uinput.KEY_C, 0) # Release Left key
+    
+  if (not R1) and (not GPIO.input(7)):  # Left button pressed
+    R1 = True
+    #print("select pressed")
+    device.emit(uinput.KEY_V, 1) # Press Left key
+  if R1 and GPIO.input(7):  # Left button released
+    R1 = False
+    device.emit(uinput.KEY_V, 0) # Release Left key
+    
+  if (not R2) and (not GPIO.input(1)):  # Left button pressed
+    R2 = True
+    #print("select pressed")
+    device.emit(uinput.KEY_N, 1) # Press Left key
+  if R2 and GPIO.input(1):  # Left button released
+    R2 = False
+    device.emit(uinput.KEY_N, 0) # Release Left key
+    
+  if (not R3) and (not GPIO.input(13)):  # Left button pressed
+    R3 = True
+    #print("select pressed")
+    device.emit(uinput.KEY_Q, 1) # Press Left key
+  if R3 and GPIO.input(13):  # Left button released
+    R3 = False
+    device.emit(uinput.KEY_Q, 0) # Release Left key
+    
+  if (not start) and (not GPIO.input(5)):  # Left button pressed
+    start = True
+    #print("select pressed")
+    device.emit(uinput.KEY_Z, 1) # Press Left key
+  if start and GPIO.input(5):  # Left button released
+    start = False
+    device.emit(uinput.KEY_Z, 0) # Release Left key
+    
+  if (not select) and (not GPIO.input(6)):  # Left button pressed
+    select = True
+    #print("select pressed")
+    device.emit(uinput.KEY_G, 1) # Press Left key
+  if select and GPIO.input(6):  # Left button released
+    select = False
+    device.emit(uinput.KEY_G, 0) # Release Left key
+    
+  if (not home) and (not GPIO.input(25)):  # Left button pressed
+    home = True
+    #print("select pressed")
+    device.emit(uinput.KEY_P, 1) # Press Left key
+  if home and GPIO.input(24):  # Left button released
+    home = False
+    device.emit(uinput.KEY_P, 0) # Release Left key
     
   time.sleep(.04)
